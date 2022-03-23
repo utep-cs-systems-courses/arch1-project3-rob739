d011c47708ec3688ce7890eefc6455db1b6177e2
Roberto Carrasco <rcarrasco8@miners.utep.edu>
2022-03-23 14:02:34 -0600

    fixed make bug

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -19,5 +19,15 @@ main()
   drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
   
+ int  col = 30;
+ int  row = 30;
+  
+  for (int j = 0; j < 50; j++) {
+    drawPixel(col,row, COLOR_RED);
+    drawPixel(col,80-row, COLOR_WHITE);
+      col ++;
+      row ++;
+  }
 }
